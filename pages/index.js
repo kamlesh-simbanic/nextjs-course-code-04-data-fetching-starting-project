06d0d1870afaa6d7ae8e09d0ab84c8a0e6bffd69
kthawani <kthawani@entera.ai>
2023-10-04 18:43:54 +0530

    initial Commit

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import fs from "fs";
+
+function HomePage(props) {
+  const { products } = props;
+
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  // fs.
+
+  return {
+    props: {
+      products: [{ id: "p1", title: "Product 1" }],
+    },
+  };
+}
+
+export default HomePage;
